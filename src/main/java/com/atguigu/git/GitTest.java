2e6fab30d4acbb3717fb43683ee1b1663d2d82af
zhla <zhla@qq.com>
2023-03-30 23:38:35 +0800

    gitee commit4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee test2");
+        System.out.println("gitee test4");
     }
 }
