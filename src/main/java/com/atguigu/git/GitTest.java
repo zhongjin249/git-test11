e48e0a8d4b828870588e3ed66d995d12a48cbabd
giteezla <z2497263639@163.com>
2023-03-30 15:44:38 +0000

    update src/main/java/com/atguigu/git/GitTest.java.
    Signed-off-by: giteezla <z2497263639@163.com>

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("gitee test2");
          System.out.println("gitee test3");
         System.out.println("gitee test4");
+         System.out.println("gitee test5");
     }
 }
