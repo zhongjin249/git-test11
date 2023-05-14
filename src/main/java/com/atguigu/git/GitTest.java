5502896760a246f354ef1fbb6f2c9409773232de
zhla <zhla@qq.com>
2023-05-14 19:13:03 +0800

    gitee commit11

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,18 +5,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("gitee test");
-        System.out.println("gitee test2");
-         System.out.println("gitee test3");
-        System.out.println("gitee test4");
-         System.out.println("gitee test5");
+
 
     }
 }
