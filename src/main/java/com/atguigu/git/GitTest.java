eebf376e68bc163bc99f6a790dd869bab3610480
zhla <zhla@qq.com>
2023-05-14 19:54:52 +0800

    github 4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,10 +7,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git1");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git3");
-        System.out.println("hello git2");
-        System.out.println("hello git6");
+
+
     }
 }
