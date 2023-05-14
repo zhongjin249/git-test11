8591fd77c7680a075d620eb249decdababc83736
zhongjin249 <128662203+zhongjin249@users.noreply.github.com>
2023-05-14 19:40:50 +0800

    Update GitTest.java
    change 2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,5 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git3");
         System.out.println("hello git2");
-        System.out.println("hello git3");
     }
 }
